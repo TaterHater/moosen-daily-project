174b208a41e2ae6952986a05e36bb0becab1e919
DerKO <dr.knockout99@gmail.com>
2022-05-26 21:20:52 -0600

    Added Prettier YAML config

--- sketch.js
+++ sketch.js
@@ -16,76 +16,76 @@ let currentTarget;
 let targets = [];
 
 function setup() {
-  const numTargets = 800;
-  const width = 800;
-  const height = 800;
+	const numTargets = 800;
+	const width = 800;
+	const height = 800;
 
-  createCanvas(width, height);
+	createCanvas(width, height);
 
-  pursuer = new Boid(
-    {
-      x: 100,
-      y: 100,
-      maxSpeed: 1.1,
-      color: color(255, 0, 0),
-      size: 9,
-      perception: 300,
-    },
-    { width, height }
-  );
+	pursuer = new Boid(
+		{
+			x: 100,
+			y: 100,
+			maxSpeed: 1.1,
+			color: color(255, 0, 0),
+			size: 9,
+			perception: 300,
+		},
+		{ width, height },
+	);
 
-  for (var i = 0; i < numTargets; i++) {
-    targets.push(
-      new Boid(
-        {
-          x: random(0, width),
-          y: random(0, height),
-          color: color(random(192, 255), random(192, 255), random(192, 255)),
-          size: random(4, 6),
-        },
-        { width, height }
-      )
-    );
-  }
-  currentTarget = targets.length - 1;
+	for (var i = 0; i < numTargets; i++) {
+		targets.push(
+			new Boid(
+				{
+					x: random(0, width),
+					y: random(0, height),
+					color: color(random(192, 255), random(192, 255), random(192, 255)),
+					size: random(4, 6),
+				},
+				{ width, height },
+			),
+		);
+	}
+	currentTarget = targets.length - 1;
 }
 
 function draw() {
-  background(20, 70, 100);
+	background(20, 70, 100);
 
-  // Targets
-  for (var i = 0; i < targets.length; i++) {
-    if (i === currentTarget) {
-      targets[i].updateColor(color(0, 255, 0));
-    }
-    if (targets[i].getDistToTarget(pursuer) <= targets[i].perception) {
-      let arrSteer = targets[i].flee(pursuer);
-      targets[i].applyForce(arrSteer);
-    } else {
-      targets[i].wander();
-    }
-    targets[i].edges();
-    targets[i].update();
-    targets[i].show();
-  }
+	// Targets
+	for (var i = 0; i < targets.length; i++) {
+		if (i === currentTarget) {
+			targets[i].updateColor(color(0, 255, 0));
+		}
+		if (targets[i].getDistToTarget(pursuer) <= targets[i].perception) {
+			let arrSteer = targets[i].flee(pursuer);
+			targets[i].applyForce(arrSteer);
+		} else {
+			targets[i].wander();
+		}
+		targets[i].edges();
+		targets[i].update();
+		targets[i].show();
+	}
 
-  // Pursuer
-  let dist = pursuer.getDistToTarget(targets[currentTarget]);
+	// Pursuer
+	let dist = pursuer.getDistToTarget(targets[currentTarget]);
 
-  if (dist < pursuer.perception) {
-    let steering = pursuer.pursue(targets[currentTarget]);
-    pursuer.applyForce(steering);
-  } else {
-    pursuer.wander();
-  }
+	if (dist < pursuer.perception) {
+		let steering = pursuer.pursue(targets[currentTarget]);
+		pursuer.applyForce(steering);
+	} else {
+		pursuer.wander();
+	}
 
-  if (dist < pursuer.r + targets[currentTarget].r) {
-    targets.pop();
-    console.log(targets.length);
-    currentTarget--;
-  }
+	if (dist < pursuer.r + targets[currentTarget].r) {
+		targets.pop();
+		console.log(targets.length);
+		currentTarget--;
+	}
 
-  pursuer.edges(); // not in video added after the fact
-  pursuer.update();
-  pursuer.show();
+	pursuer.edges(); // not in video added after the fact
+	pursuer.update();
+	pursuer.show();
 }
